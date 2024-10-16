076fc83334b0d07bbbad2a8c65fc6406e953da81
Adeoluw <104023937+Adeoluw@users.noreply.github.com>
2024-10-16 06:06:18 -0700

    continuation

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const processCards = document.getElementsByClassName("process-card");
+const mobileMenu = document.querySelector(".mobile-menu")
+const nav = document.querySelector(".nav")
+const closeNav = document.querySelector(".close-menu")
 
 for (let i = 0; i < processCards.length; i++) {
   processCards[i].addEventListener("mouseenter", () => {
@@ -12,3 +15,19 @@ for (let i = 0; i < processCards.length; i++) {
   });
 }
 
+mobileMenu.addEventListener("click", () => {
+  nav.style.display = "block"
+  nav.style.right = "0"
+  nav.style.transition = "all 1s ease"
+  mobileMenu.style.display = "none"
+  nav.classList.add("mobile-nav")
+})
+
+closeNav.addEventListener("click", () => {
+  nav.style.display = "none"
+  mobileMenu.style.display = "block"
+  nav.classList.remove("mobile-nav")
+
+  console.log("ligma")
+
+})
