30a9c657cfa1f631d374ed45d07d2eb793e5cc6a
Adeoluw <104023937+Adeoluw@users.noreply.github.com>
2024-11-11 04:55:56 -0800

    little animation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const processCards = document.getElementsByClassName("process-card");
-const mobileMenu = document.querySelector(".mobile-menu")
-const nav = document.querySelector(".nav")
-const closeNav = document.querySelector(".close-menu")
+const mobileMenu = document.querySelector(".mobile-menu");
+const nav = document.querySelector(".nav");
+const closeNav = document.querySelector(".close-menu");
 
 for (let i = 0; i < processCards.length; i++) {
   processCards[i].addEventListener("mouseenter", () => {
@@ -16,26 +16,38 @@ for (let i = 0; i < processCards.length; i++) {
 }
 
 mobileMenu.addEventListener("click", () => {
-  nav.style.display = "block"
-  nav.style.right = "0"
-  nav.style.transition = "all 1s ease"
-  mobileMenu.style.display = "none"
-  nav.classList.add("mobile-nav")
-})
+  nav.style.display = "block";
+  nav.style.right = "0";
+  nav.style.position = "absolute";
+  nav.style.opacity = "1"
+  mobileMenu.style.display = "none";
+  setTimeout(() => {
+    nav.style.transition = "all 1s ease";
+    nav.classList.add("mobile-nav");
+  }, 100);
+});
 
 closeNav.addEventListener("click", () => {
-  nav.style.display = "none"
-  mobileMenu.style.display = "block"
-  nav.classList.remove("mobile-nav")
+  setTimeout(() => {
+    nav.style.display = "none";
+  }, 500);
+  nav.style.opacity = "0"
+  nav.style.top = "0"
+  // nav.style.transform = "translateX(90px)"
+  mobileMenu.style.display = "block";
+  nav.classList.remove("mobile-nav");
 
   // console.log("ligma")
-
-})
+});
 
 document.addEventListener("click", (e) => {
-  if (!nav.contains(e.target) && !closeNav.contains(e.target)&& !mobileMenu.contains(e.target)) {
-    nav.style.display = "none"
-    mobileMenu.style.display = "block"
-    nav.classList.remove("mobile-nav")
+  if (
+    !nav.contains(e.target) &&
+    !closeNav.contains(e.target) &&
+    !mobileMenu.contains(e.target)
+  ) {
+    nav.style.display = "none";
+    mobileMenu.style.display = "block";
+    nav.classList.remove("mobile-nav");
   }
 });
